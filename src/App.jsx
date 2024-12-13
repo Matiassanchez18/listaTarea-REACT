873e93b11def4d1c050ca356a9769642848a8ed6
Matias <matiassanchez13422@gmail.com>
2024-12-13 18:36:11 -0300

    Se agregaron las validaciones al form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import FormularioTarea from './assets/components/FormularioTarea'
 import "bootstrap/dist/css/bootstrap.min.css";
-import ListaTarea from './assets/components/ListaTarea';
+
 
 function App() {
   
--- src/assets/components/FormularioTarea.jsx
+++ src/assets/components/FormularioTarea.jsx
@@ -3,19 +3,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import ListaTarea from "./ListaTarea"
 import { PlusCircle } from "react-bootstrap-icons";
-import ListaTarea from "./ListaTarea";
+import { useForm } from "react-hook-form";
 
 const FormularioTarea = () => {
+  // formulario requisitos
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+const enviadoForm = (data)=> console.log(data)
+
+
   return (
     <section>
       <h1 className="text-center mt-5 border-bottom ">Lista de tarea</h1>
-      <form action="" className="w-50 mx-auto px-4 mt-5">
+      <form action="" className="w-50 mx-auto px-4 mt-5"  onSubmit={handleSubmit(enviadoForm)}>
         <InputGroup className="mb-3">
           <Form.Control
             className="bg-white text-dark"
             placeholder="Ingrese su tarea"
             aria-describedby="basic-addon2"
+           {...register("tarea",{
+            required: "Este campo es obligatorio",
+            minLength: {
+              value: 3,
+              message: "El minimo de caracteres es de 3",
+            },
+            maxLength:{
+              value: 15,
+              message: "El maximo de caracteres permitido es de 15",
+            },
+           })}
           />
           <Button
             variant="outline-secondary"
@@ -26,8 +48,10 @@ const FormularioTarea = () => {
             <PlusCircle></PlusCircle>
           </Button>
         </InputGroup>
+        <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
+        
       </form>
-      <ListaTarea></ListaTarea>
+      <ListaTarea className="mt-3"></ListaTarea>
     </section>
   );
 };
