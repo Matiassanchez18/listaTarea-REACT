43b7108905acc3297aa4ed87336fd0f435d5ef7b
Matias <matiassanchez13422@gmail.com>
2024-12-13 19:51:09 -0300

    Se agrego comentarios

--- src/assets/components/FormularioTarea.jsx
+++ src/assets/components/FormularioTarea.jsx
@@ -20,6 +20,7 @@ const FormularioTarea = () => {
   const [arrayTareas, setarrayTareas] = useState([])
 
 
+  // aqui data se encarga de recibir lo que se escribio en el form, la variable enviadoForm es la que sucede cuando se toca el boton enviar, some sirve para ir objeto por objeto en el array, setarrayTareas es la funcion que ayuda a guardar los objetos en el array y adentro se la llama a la variante y a los datos que se necesitan agregar,reset se usa para resetear el form.
 const enviadoForm = (data)=>{
   if (arrayTareas.some((itarea) => itarea === data.tarea.trim())) {
     alert('la tarea ya existe')
@@ -34,12 +35,14 @@ const enviadoForm = (data)=>{
   return (
     <section>
       <h1 className="text-center mt-5 border-bottom ">Lista de tarea</h1>
+      {/* onSubmit = crea el evento del click en el boton, el handlesubmit se usa para prevenir el enviado y (enviadoForm) llama a la funcion que hace lo que queremos cuando se envie el form */}
       <form action="" className="w-50 mx-auto px-4 mt-5"  onSubmit={handleSubmit(enviadoForm)}>
         <InputGroup className="mb-3">
           <Form.Control
             className="bg-white text-dark"
             placeholder="Ingrese su tarea"
             aria-describedby="basic-addon2"
+            // {...register} es para agregar validaciones al form, "tarea" es como queremos que se llame el gurdado de los datos obtenidos en el input
            {...register("tarea",{
             required: "Este campo es obligatorio",
             minLength: {
@@ -61,9 +64,11 @@ const enviadoForm = (data)=>{
             <PlusCircle></PlusCircle>
           </Button>
         </InputGroup>
+        {/* Form.text es para agregar el mensaje de error cuando no se cumple las validaciones */}
         <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
         
       </form>
+      {/* aqui se envia la informacion del array al componente que dibuja los slots de las acciones, se puede mandar informacion de padre a hijo */}
       <ListaTarea className="mt-3" arrayTareas={arrayTareas}></ListaTarea>
     </section>
   );
--- src/assets/components/ListaTarea.jsx
+++ src/assets/components/ListaTarea.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tarea from './Tarea'
 
+// dentro de los () va la informacion que recibe el componente hijo
 const ListaTarea = ({arrayTareas}) => {
     return (
         <section>
+            {/* .map recorre todos los espacio que tiene el array y dibujo 1 slot de tarea x las tareas que hay en el array, se le envia los datos tarea para iterar los objetos dentro del array, y posicion ya que el map tiene la posicion de cada uno de los objetos y este no se repite, ademas es necesaria la posicion porque react necesita un identifucador unico por cada componente */}
         <article>
           {arrayTareas.map((tarea, posicion) => (
             <Tarea key={posicion} tarea={tarea}/>
